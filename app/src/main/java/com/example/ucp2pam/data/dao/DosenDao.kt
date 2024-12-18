15afc04d8cfd77ec0a064d7568a3cdf99061d3d2
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-18 10:05:19 +0700

    menambahkan Dao Dosen | DosenDao.kt

---
+++ app/src/main/java/com/example/ucp2pam/data/dao/DosenDao.kt
@@ -0,0 +1,28 @@
+package com.example.ucp2pam.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2pam.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DosenDao {
+
+    @Insert
+    suspend fun insertDosen(dosen: Dosen)
+
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getDosen(nidn: String): Flow<Dosen>
+
+    @Delete
+    suspend fun deleteDosen(dosen: Dosen)
+
+    @Update
+    suspend fun updateDosen(dosen: Dosen)
+}
