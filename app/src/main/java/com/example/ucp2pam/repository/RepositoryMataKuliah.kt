3cd0fb0a594b2931916cc296648baae13482ff34
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-24 00:06:40 +0700

    Fix error dan penyempurnaan kode MataKuliah | RepositoryMataKuliah

--- app/src/main/java/com/example/ucp2pam/repository/RepositoryMataKuliah.kt
+++ app/src/main/java/com/example/ucp2pam/repository/RepositoryMataKuliah.kt
@@ -4,11 +4,14 @@ import com.example.ucp2pam.data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMataKuliah {
-    suspend fun insertMataKuliah(mataKuliah: MataKuliah)
-    fun getAllMataKuliah(): Flow<List<MataKuliah>>
-    fun getMataKuliah(kode: String): Flow<MataKuliah>
-    fun getMataKuliahBySemester(semester: Int): Flow<List<MataKuliah>>
-    fun getMataKuliahByDosen(dosenPengampu: String): Flow<List<MataKuliah>>
-    suspend fun deleteMataKuliah(mataKuliah: MataKuliah)
-    suspend fun updateMataKuliah(mataKuliah: MataKuliah)
+        suspend fun insertMataKuliah(matakuliah: MataKuliah)
+
+        fun getAllMataKuliah(): Flow<List<MataKuliah>>
+
+        suspend fun updateMataKuliah(matakuliah: MataKuliah)
+
+        suspend fun deleteMataKuliah(matakuliah: MataKuliah)
+
+        fun getDetailMataKuliah(kode: String): Flow<MataKuliah>
 }
+
