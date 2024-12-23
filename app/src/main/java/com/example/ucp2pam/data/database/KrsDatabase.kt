c541197193f05043950f486f6806e733c6ed9d1b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-23 22:09:04 +0700

    Fix error dan penyempurnaan kode KrsDatabase | KrsDatabase

--- app/src/main/java/com/example/ucp2pam/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/ucp2pam/data/database/KrsDatabase.kt
@@ -1,6 +1,5 @@
 package com.example.ucp2pam.data.database
 
-import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -8,10 +7,8 @@ import com.example.ucp2pam.data.dao.DosenDao
 import com.example.ucp2pam.data.dao.MataKuliahDao
 import com.example.ucp2pam.data.entity.Dosen
 import com.example.ucp2pam.data.entity.MataKuliah
-
 @Database(entities = [Dosen::class, MataKuliah::class], version = 1, exportSchema = false)
 abstract class KrsDatabase : RoomDatabase() {
-
     abstract fun dosenDao(): DosenDao
     abstract fun mataKuliahDao(): MataKuliahDao
 
@@ -19,15 +16,15 @@ abstract class KrsDatabase : RoomDatabase() {
         @Volatile
         private var Instance: KrsDatabase? = null
 
-        fun getDatabase(context: Context): KrsDatabase {
-            return (Instance ?: synchronized(this) {
+        fun getDatabase(context: android.content.Context): KrsDatabase {
+            return Instance ?: synchronized(this) {
                 Room.databaseBuilder(
-                    context,
+                    context.applicationContext,
                     KrsDatabase::class.java,
-                    "krsDatabase"
+                    "KrsDatabase"
                 )
                     .build().also { Instance = it }
-            })
+            }
         }
     }
 }
