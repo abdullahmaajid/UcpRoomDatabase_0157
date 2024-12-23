6122bd55b2dfe0efc8d687875a745001b12d3f90
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-24 05:50:50 +0700

    Fix error dan penyempurnaan kode MainActivity.kt | MainActivity.kt

--- app/src/main/java/com/example/ucp2pam/MainActivity.kt
+++ app/src/main/java/com/example/ucp2pam/MainActivity.kt
@@ -11,7 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.ucp2pam.ui.navigation.PengelolaHalaman
+
 import com.example.ucp2pam.ui.theme.UCP2PAMTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +26,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             UCP2PAMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PengelolaHalaman(modifier = Modifier.padding(innerPadding))
+
                 }
             }
         }
