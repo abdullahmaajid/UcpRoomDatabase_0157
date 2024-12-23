be22cf1b422e1382da53e6adf06427a99e64485b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-24 03:22:53 +0700

    Fix error dan penyempurnaan kode InsertMkView.kt | InsertMkView.kt

---
+++ app/src/main/java/com/example/ucp2pam/ui/view/matakuliah/InsertMkView.kt
@@ -0,0 +1,281 @@
+package com.example.ucp2pam.ui.view.matakuliah
+
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2pam.data.entity.Dosen
+import com.example.ucp2pam.ui.customwidget.CustomTopAppBar
+import com.example.ucp2pam.ui.navigation.AlamatNavigasi
+import com.example.ucp2pam.ui.viewmodel.PenyediaViewModel
+import com.example.ucp2pam.ui.viewmodel.matakuliah.FormErrorState
+import com.example.ucp2pam.ui.viewmodel.matakuliah.MataKuliahEvent
+import com.example.ucp2pam.ui.viewmodel.matakuliah.MataKuliahUiState
+import com.example.ucp2pam.ui.viewmodel.matakuliah.MkViewModel
+import kotlinx.coroutines.launch
+
+
+
+object DestinasiInsertMataKuliah : AlamatNavigasi {
+    override val route: String = "insert_mk"
+}
+
+@Composable
+fun InsertMkView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: MkViewModel = viewModel (factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.uiState // Ambil UI state dari ViewModel
+    val snackbarHostState = remember{ SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    //Observasi perubahan snackbarMessage
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+
+            CustomTopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Mata Kuliah"
+            )
+
+            //Isi Body
+            InsertBodyMk(
+                uiState = uiState,
+                dosenList = uiState.dosentList,  // Pastikan ini ada
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.saveData() //Simpan data
+                    }
+                    onNavigate()
+                }
+            )
+
+        }
+    }
+}
+
+@Composable
+fun InsertBodyMk(
+    modifier: Modifier = Modifier,
+    onValueChange: (MataKuliahEvent) -> Unit,
+    uiState: MataKuliahUiState,
+    onClick: () -> Unit,
+    dosenList: List<Dosen>
+){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMk(
+            mataKuliahEvent = uiState.mataKuliahEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth(),
+            dosenList = dosenList
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Simpan")
+        }
+    }
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormMk(
+    mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
+    onValueChange: (MataKuliahEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier,
+    dosenList: List<Dosen>
+) {
+    val semester = listOf("Ganjil", "Genap")
+    val jenis = listOf("Mata Kuliah Wajib", "Mata Kuliah Pilihan")
+
+    var expanded by remember { mutableStateOf(false) }
+    var chosenDropdown by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.nama,
+            onValueChange = {
+                onValueChange(mataKuliahEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan Nama") },
+        )
+        Text(text = errorState.nama ?: "", color = Color.Red)
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.kode, onValueChange = {
+                onValueChange(mataKuliahEvent.copy(kode = it))
+            },
+            label = { Text("Kode") },
+            isError = errorState.kode != null,
+            placeholder = { Text("Masukkan Kode") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.kode ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Semester")
+        Row(modifier = Modifier.fillMaxWidth()) {
+            semester.forEach { sr ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = mataKuliahEvent.semester == sr,
+                        onClick = { onValueChange(mataKuliahEvent.copy(semester = sr)) }
+                    )
+                    Text(text = sr)
+                }
+            }
+        }
+        Text(text = errorState.semester ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.sks,
+            onValueChange = {
+                onValueChange(mataKuliahEvent.copy(sks = it))
+            },
+            label = { Text("Sks") },
+            isError = errorState.sks != null,
+            placeholder = { Text("Masukkan Sks") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.sks ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Jenis")
+        Row {
+            jenis.forEach { jenis ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = mataKuliahEvent.jenis == jenis,
+                        onClick = {
+                            onValueChange(mataKuliahEvent.copy(jenis = jenis))
+                        },
+
+                        )
+                    Text(
+                        text = jenis,
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            OutlinedTextField(
+                value = chosenDropdown,
+                onValueChange = { },
+                label = { Text("Pilih Dosen Pengampu") },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                },
+                modifier = Modifier.menuAnchor().fillMaxWidth(),
+                readOnly = true,
+                isError = errorState.dosenPengampu != null
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                dosenList.forEach { dosen ->
+                    DropdownMenuItem(
+                        onClick = {
+                            chosenDropdown = dosen.nama
+                            expanded = false
+                            onValueChange(mataKuliahEvent.copy(dosenPengampu = dosen.nama))
+                        },
+                        text = { Text(text = dosen.nama) }
+                    )
+                }
+            }
+        }
+        Text(text = errorState.dosenPengampu ?: "", color = Color.Red)
+    }
+}
